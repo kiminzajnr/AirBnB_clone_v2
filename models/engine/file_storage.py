935d3e10fdadd7e469db87a4bbcaa94a4b467a7f
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-10 14:42:03 +0300

    add new public instance method delete, update prototype of all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        one_type_objects = {}
+        if cls is not None:
+            for key, value in self.__objects.items():
+                if key.startswith(str(cls.__name__)):
+                    one_type_objects[key] = value
+        one_type_objects = self.__objects
+        return one_type_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +30,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """to delete obj from __objects if it’s inside -
+        if obj is equal to None, the method should not do anything"""
+        if obj is not None and obj in self.__objects:
+            del obj
 
     def reload(self):
         """Loads storage dictionary from file"""
