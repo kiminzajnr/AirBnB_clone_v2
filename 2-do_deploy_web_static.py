e5bae7d16015528c206393fe6bcddb55f0b13cb5
kiminzajnr <kiminzaeric@gmail.com>
2023-10-07 16:00:09 +0300

    -

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,47 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers,
-using the function do_deploy
-"""
-import time
-import os
-from fabric.api import *
-from fabric.operations import run, put
 
+"""
+ Fabric script that generates a .tgz archive from the contents of the
+ web_static folder of your AirBnB Clone repo, using the function do_pack.
+"""
+
+from fabric.api import run, env, run, put, sudo, local
+import os.path
+from datetime import datetime
+""" Importing the required modules"""
+
+
+def do_pack():
+    """ Function to help to generate the archive"""
+
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    hour = now.strftime("%H")
+    minute = now.strftime("%M")
+    second = now.strftime("%S")
+
+    local("mkdir -p versions")
+
+    archive_name = f"versions/web_static_{year}{month}{day}{hour}\
+{minute}{second}.tgz"
+    # capture the result
+    result = local(f"tar -czvf {archive_name} web_static", capture=True)
+
+    if result.failed:
+        return None
+    else:
+        return archive_name
+
+
+"""
+Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy:
+"""
 
 env.hosts = ['100.26.243.45', '54.236.24.205']
 
-def do_pack():
-    """generates a .tgz archive"""
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return "versions/web_static_{:s}.tgz".\
-            format(time.strftime("%Y%m%d%H%M%S"))
-    except BaseException:
-        return None
-
-
-# def do_deploy(archive_path):
-#     """distributes an archive to my web servers"""
-#     if not os.path.exists(archive_path):
-#         return False
-#     try:
-#         # Upload archive
-#         put(archive_path, '/tmp/')
-#         archive_filename = archive_path.split('/')[-1]
-#         archive_name_noext = archive_filename.split('.')[0]
-#         release_path = f"/data/web_static/releases/{archive_name_noext}"
-#         run(f"mkdir -p {release_path}")
-#         run(f"tar -xzf /tmp/{archive_filename} -C {release_path}")
-
-#         run(f"rm /tmp/{archive_filename}")
-
-#         run("rm /data/web_static/current")
-
-#         run(f"ln -s {release_path} /data/web_static/current")
-
-#         return True
-#     except Exception as e:
-#         return False
 
 def do_deploy(archive_path):
     """ Distributes an archive to the web servers"""
@@ -56,8 +53,7 @@ def do_deploy(archive_path):
         # <archive filename without extension> on the web server
         archive_filename = archive_path.split("/")[-1]
         archive_name = archive_filename.split(".")[0]
-        remove_folder = f'/data/web_static/releases/\
-            {archive_name}/web_static/*'
+        remove_folder = '/data/web_static/releases/{archive_name}/web_static/*'
         # check if the code is running locally or on remote hosts
         run_locally = os.getenv("run_locally", None)
         if run_locally is None:
@@ -66,8 +62,7 @@ def do_deploy(archive_path):
             local(f'tar -xzf {archive_path} -C\
  /data/web_static/releases/{archive_name}/')
 
-            local(f'mv /data/web_static/releases/\
-{archive_name}/web_static/* '
+            local(f'mv /data/web_static/releases/{archive_name}/web_static/* '
                   f'/data/web_static/releases/{archive_name}/')
             local(f'rm -rf {remove_folder}')
             local(f'rm -rf /data/web_static/current')
